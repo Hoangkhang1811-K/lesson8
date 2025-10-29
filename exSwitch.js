72227cea3c479f79a8fed3efec5758b377ce4f5f
unknown <dhkhang18112000.com>
2025-10-29 12:53:08 +0700

    exSwitch

--- exSwitch.js
+++ exSwitch.js
@@ -12,3 +12,28 @@ switch (browser) {
     default:
         alert('We hope that this page looks ok!');
 }
+
+
+let a = +prompt('a?', '');
+switch (a) {
+    case 0:
+        alert(0);
+        break;
+    case 1:
+        alert(1);
+        break;
+    case 2:
+    case 3:
+        alert('2,3');
+        break;
+}
+
+
+let value = prompt('Type a number', 0);
+if (value > 0) {
+    alert(1);
+} else if (value < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
