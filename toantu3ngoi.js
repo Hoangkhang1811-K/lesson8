675607c94d07bf49fc98f08c5c739dc6b55d710c
unknown <dhkhang18112000.com>
2025-10-29 13:09:35 +0700

    toantungoi

---
+++ toantu3ngoi.js
@@ -0,0 +1,14 @@
+let a = prompt("a: ");
+let b = prompt("b: ");
+result =(a+b <4)? 'below' : 'over';
+document.write(result);
+
+let login = prompt("Enter:");
+let message = (login == 'Employee') ?
+    'Hello' :
+    (login == 'Director') ?
+        'Greetings' :
+        (login == '') ?
+            'No login' :
+            '';
+alert(message);
